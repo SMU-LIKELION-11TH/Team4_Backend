70acfdd5ae58c83e34e884b48a2597e01374a5bb
pmsu2007 <pmsu2008@gmail.com>
2023-07-04 01:53:23 +0900

    Feat : 초기 초기 프로젝트 설정

---
+++ Traditional-Market/src/main/java/smu/likelion/Traditional/Market/TraditionalMarketApplication.java
@@ -0,0 +1,13 @@
+package smu.likelion.Traditional.Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraditionalMarketApplication.class, args);
+	}
+
+}
