a1bcbf2fca977abdca2174828be086cdd88a069f
Jiyun82 <202110918@sangmyung.kr>
2023-07-06 22:56:26 +0900

    이미지 파일 업로드 구현

---
+++ Traditional-Market/src/main/java/smu/likelion/Traditional/Market/domain/UploadFile.java
@@ -0,0 +1,13 @@
+package smu.likelion.Traditional.Market.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UploadFile {
+
+    private String uploadFilename;
+
+    private String storeFilename;
+}
